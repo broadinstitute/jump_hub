97a60cd4902316d1e5f1c93d6be9d86c2e520b4d
Alán F. Muñoz <afer.mg@gmail.com>
2025-08-14 17:32:16 -0400

    build(nix): replace libz -> zlib and format

--- flake.nix
+++ flake.nix
@@ -7,70 +7,83 @@
     flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs, flake-utils, systems, ... } @ inputs:
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs = import nixpkgs {
-              system = system;
-              config.allowUnfree = true;
-            };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      systems,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
 
-            mpkgs = import inputs.nixpkgs_master {
-              system = system;
-              config.allowUnfree = true;
-            };
+        mpkgs = import inputs.nixpkgs_master {
+          system = system;
+          config.allowUnfree = true;
+        };
 
-            libList = [
-                # Add needed packages here
-                pkgs.libz # Numpy
-                pkgs.stdenv.cc.cc
-                # pkgs.libGL
-                # pkgs.glib
-                # pkgs.ruff
-                # pkgs.ruff-lsp
-              ];
-          in
-          with pkgs;
-        {
-          devShells = {
-            default  = let
+        libList = [
+          # Add needed packages here
+          pkgs.zlib # Numpy
+          pkgs.stdenv.cc.cc
+          # pkgs.libGL
+          # pkgs.glib
+          # pkgs.ruff
+          # pkgs.ruff-lsp
+        ];
+      in
+      with pkgs;
+      {
+        devShells = {
+          default =
+            let
               # These packages get built by Nix, and will be ahead on the PATH
-                pwp = (python311.withPackages (p: with p; [
-                   ]));
-            in mkShell {
-               NIX_LD = runCommand "ld.so" {} ''
-                        ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
-                      '';
-                NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
-                packages = [
-                    which
-                    uv
-                    quartoMinimal
-                    marksman # markdown LSP
-                    pwp
-                    python311Packages.venvShellHook # This has to be here
-                ]
-                ++ libList;
-                venvDir = "./.venv";
-                postVenvCreation = ''
-                    unset SOURCE_DATE_EPOCH
-                  '';
-                postShellHook = ''
-                    unset SOURCE_DATE_EPOCH
-                  '';
-                shellHook = ''
-                    export UV_PYTHON=${pkgs.python311}
-                    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
-                    export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+              pwp = (
+                python311.withPackages (
+                  p: with p; [
+                  ]
+                )
+              );
+            in
+            mkShell {
+              NIX_LD = runCommand "ld.so" { } ''
+                ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+              '';
+              NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
+              packages = [
+                which
+                uv
+                quartoMinimal
+                marksman # markdown LSP
+                pwp
+                python311Packages.venvShellHook # This has to be here
+              ] ++ libList;
+              venvDir = "./.venv";
+              postVenvCreation = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              postShellHook = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              shellHook = ''
+                export UV_PYTHON=${pkgs.python311}
+                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+                export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
 
-                    uv sync --group dev
+                uv sync --group dev
 
-                    runHook venvShellHook
-                    export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
-                    export QUARTO_PYTHON=.venv/bin/python
-                '';
-             };
-          };
-        }
-      );
+                runHook venvShellHook
+                export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
+                export QUARTO_PYTHON=.venv/bin/python
+              '';
+            };
+        };
+      }
+    );
 }
